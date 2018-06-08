6a0a734971cb1a684637ed62e9762b9b945bd1f9
Angelina Blyth <angelinalblyth@hotmail.com>
2018-06-08 21:43:12 +0100

    Added more bookings

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@ Member.delete_all()
 
 member1 = Member.new({'first_name' => 'Angelina', 'last_name' => 'Blyth', 'membership_type' => 'Basic', 'date_of_birth' => '17-04-1990'})
 member1.save()
-member2 = Member.new({'first_name' => 'Grant', 'last_name' => 'McPhilemy', 'membership_type' => 'Basic', 'date_of_birth' => '24-09-1992'})
+member2 = Member.new({'first_name' => 'Christine', 'last_name' => 'Weirich', 'membership_type' => 'Basic', 'date_of_birth' => '24-09-1992'})
 member2.save()
 member3 = Member.new({'first_name' => 'Louise', 'last_name' => 'Reekie', 'membership_type' => 'Basic', 'date_of_birth' => '30-10-1988'})
 member3.save()
@@ -26,6 +26,18 @@ gymclass4.save()
 
 booking1 = Booking.new({'member_id' => member1.id, 'gym_class_id' => gymclass3.id})
 booking1.save()
+booking2 = Booking.new({'member_id' => member4.id, 'gym_class_id' => gymclass3.id})
+booking2.save()
+booking3 = Booking.new({'member_id' => member2.id, 'gym_class_id' => gymclass2.id})
+booking3.save()
+booking4 = Booking.new({'member_id' => member3.id, 'gym_class_id' => gymclass3.id})
+booking4.save()
+booking5 = Booking.new({'member_id' => member1.id, 'gym_class_id' => gymclass4.id})
+booking5.save()
+booking6 = Booking.new({'member_id' => member4.id, 'gym_class_id' => gymclass1.id})
+booking6.save()
+booking7 = Booking.new({'member_id' => member2.id, 'gym_class_id' => gymclass3.id})
+booking7.save()
 
 binding.pry
 nil
