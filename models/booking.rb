7c74dca4a30ae2fcd59616467dfe488edb84df58
Angelina Blyth <angelinalblyth@hotmail.com>
2018-06-08 21:09:00 +0100

    Added CRUF for bookings

---
+++ models/booking.rb
@@ -0,0 +1,40 @@
+require_relative( '../db/sql_runner' )
+
+class Booking
+
+attr_reader :id, :member_id, :gym_class_id
+
+def initialize(options)
+  @id = options['id'].to_i if options['id']
+  @member_id = options['member_id'].to_i
+  @gym_class_id = options['gym_class_id'].to_i
+end
+
+# Saves a new booking
+def save()
+  sql = "INSERT INTO bookings(member_id, gym_class_id) VALUES ($1, $2) RETURNING id"
+  values = [@member_id, @gym_class_id]
+  results = SqlRunner.run(sql, values)
+  @id = results.first()['id'].to_i
+end
+
+#Shows all bookings
+def self.all()
+  sql = "SELECT * FROM bookings"
+  results = SqlRunner.run(sql)
+  return results.map {|booking| Booking.new(booking)}
+end
+
+def self.delete_all()
+  sql = "DELETE FROM bookings"
+  SqlRunner.run(sql)
+end
+
+#Deletes a booking
+def self.delete(id)
+  sql = "DELETE FROM bookings WHERE id = $1"
+  values = [id]
+  SqlRunner.run(sql, values)
+end
+
+end
