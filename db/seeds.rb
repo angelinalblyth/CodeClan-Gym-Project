11ab998070b79752fb47c0ea803ea23b939873a8
Angelina Blyth <angelinalblyth@hotmail.com>
2018-06-08 21:15:59 +0100

    Members table populated with details from seed.

---
+++ db/seeds.rb
@@ -0,0 +1,14 @@
+require("pry-byebug")
+
+require_relative("../models/member.rb")
+require_relative("../models/gym_class.rb")
+require_relative("../models/booking.rb")
+
+member1 = Member.new({'first_name' => 'Angelina', 'last_name' => 'Blyth', 'membership_type' => 'Basic', 'date_of_birth' => '17-04-1990'})
+member1.save()
+member2 = Member.new({'first_name' => 'Grant', 'last_name' => 'McPhilemy', 'membership_type' => 'Basic', 'date_of_birth' => '24-09-1992'})
+member2.save()
+member3 = Member.new({'first_name' => 'Louise', 'last_name' => 'Reekie', 'membership_type' => 'Basic', 'date_of_birth' => '30-10-1988'})
+member3.save()
+member4 = Member.new({'first_name' => 'Christy', 'last_name' => 'Whyte', 'membership_type' => 'Basic', 'date_of_birth' => '21-01-1975'})
+member4.save()
