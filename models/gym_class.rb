42e1bbbfd2579ff6f7b4f4dd7842b624e7867440
Angelina Blyth <angelinalblyth@hotmail.com>
2018-06-12 14:32:55 +0100

    changed margin

--- models/gym_class.rb
+++ models/gym_class.rb
@@ -13,7 +13,7 @@ def initialize(options)
   @cost = options['cost'].to_i
   @duration = options['duration'].to_i
   @class_time = options['class_time']
-  @capacity = options['capacity']ß
+  @capacity = options['capacity'].to_i
 end
 
   #Create a new class for the gym
